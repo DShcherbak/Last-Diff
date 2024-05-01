6cdd95d848bf49cb5df5be06732e94d86ddd89bd
Denys Shcherbak <denys.shcherbak@streamlineplan.com>
2024-05-01 22:17:04 +0300

    new test

---
+++ src/tests/after-2.cpp
@@ -0,0 +1,24 @@
+﻿int add(int x, int y) {
+    return x + y;
+}
+
+auto mult(int x, int y) -> int {
+    return x * y; 
+}
+
+void stable() {
+    std::cout << "nothing." << std::endl;
+}
+
+int hypothesis()
+{
+  int a = 3;
+  int b = 4;
+  return std::sqrt(a * a + b * b);
+}
+
+int perform(int x) 
+{
+  int c = hypothesis();
+  return x + c;
+}
---
+++ src/tests/before-2.cpp
@@ -0,0 +1,15 @@
+﻿int mult(int x, int y) { return x * y; }
+
+int add(int x, int y) {return x + y;       }
+
+void stable() {std::cout << "nothing." << std::endl;}
+
+void removed() {}
+ 
+int perform(int x) {
+    int a = 3;
+    int b = 4;
+    int c = std::sqrt(a * a + b * b);
+
+    return x + c;
+}
